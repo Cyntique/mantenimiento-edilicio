65d1e1a43a63e7006c39f2b21dd0887ccb48749e
La Cynty <gcbtecno@gmail.com>
2020-08-03 14:11:07 -0300

    Primer commit

---
+++ assets/js/theme.js
@@ -0,0 +1,73 @@
+(function($) {
+    "use_strict";
+    
+       
+  // PRELOADER
+    $(window).on('load', function() {
+        $('#page-loader').fadeOut('slow', function() {
+            $(this).remove();
+        });
+    });
+
+    
+  // SCROLL TO TOP
+  
+  $(window).on('scroll', function () {
+    if ($(window).scrollTop() > 70) {
+        $('.scroll-to-top').addClass('reveal');
+    } else {
+        $('.scroll-to-top').removeClass('reveal');
+    }
+});
+ 
+ 
+/* ----------------------------------------------------------- */
+    /*  Fixed header
+    /* ----------------------------------------------------------- */
+
+
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() > 70) {
+            $('.site-navigation,.trans-navigation').addClass('header-white');
+        } else {
+            $('.site-navigation,.trans-navigation').removeClass('header-white');
+        }
+
+    });
+
+
+    
+  // Smooth scrolling using jQuery easing
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+    
+    
+    $('a.js-scroll-trigger').on('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').on('click', function(event) {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  
+ 
+
+})(jQuery); // End of use strict
+$(function() {
+var selectedClass = "";
+$(".filter").click(function(){
+selectedClass = $(this).attr("data-rel");
+$("#gallery").fadeTo(100, 0.1);
+$("#gallery div").not("."+selectedClass).fadeOut().removeClass('animation');
+setTimeout(function() {
+$("."+selectedClass).fadeIn().addClass('animation');
+$("#gallery").fadeTo(300, 1);
+}, 300);
+});
+});
